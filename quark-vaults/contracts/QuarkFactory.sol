f97638ca107d33d048ee7d531cee071c39ea5b13
Luiz <lvj253@gmail.com>
2024-10-19 08:55:46 -0700

    layerzero start

---
+++ quark-vaults/contracts/QuarkFactory.sol
@@ -0,0 +1,192 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/utils/Create2.sol";
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+import { VaultHubChainAccount } from "./VaultHubChainAccount.sol";
+import { SecuritySource } from "./SecuritySource.sol";
+
+import { OApp, MessagingFee, Origin } from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
+import { MessagingReceipt } from "@layerzerolabs/oapp-evm/contracts/oapp/OAppSender.sol";
+import { OAppOptionsType3 } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OAppOptionsType3.sol";
+import { OptionsBuilder } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
+
+interface IRegistryHubChain {
+    function createHubChainAccount(address _initialOwner, address _endpoint) external returns (address);
+}
+
+//import { console } from "forge-std/Test.sol";
+
+// contract VaultHubChainFactory is Ownable, OApp, OAppOptionsType3, ERC721 {
+contract QuarkFactory is Ownable, OApp, OAppOptionsType3,  ERC721 {
+
+    error InitializationFailed();
+
+    using OptionsBuilder for bytes;
+
+    address lzEndpoint;
+
+    uint256 public vaultCounter;
+
+    SecuritySource public securitySource;
+    
+    ERC20 public currencyToken;
+    mapping(uint256 => address) public vaultHubChainAccounts;
+
+    // not ERC6551
+    IRegistryHubChain registry;
+
+    uint16 public constant SEND = 1;
+    uint16 public constant SEND_ABA = 2;
+
+    //omnichain mappings
+    mapping(uint256 => address) public spokeChainsRegistries; // ChainId to RegistrySpokeChain
+    //mapping(uint256 => address) public spokeChainsImplementations; // ChainId to VaultSpokeChainAccount
+
+    mapping(uint256 => uint32) public spokeChainsIds; // ChainId to Eid
+
+
+    //events
+    event VaultCreated(address indexed owner, uint256 indexed vaultId, address indexed vaultAccount);
+    event SecuritySourceSet(address indexed securitySource);
+    event SpokeChainRegistered(uint256 indexed vaultId, uint256 chainId, uint64 nonce);
+
+    //TODO OApp initializer
+    constructor(address _initialOwner,  address _registry, address _currency, address _endpoint)  OApp(_endpoint, _initialOwner) ERC721("VaultHubChain", "VHC") Ownable(_initialOwner)  {
+        currencyToken = ERC20(_currency);
+        lzEndpoint = _endpoint;
+        registry = IRegistryHubChain(_registry);
+    }
+
+    function setSecuritySourceHubchain(address _securitySource) public onlyOwner {
+        securitySource = SecuritySource(_securitySource);
+        emit SecuritySourceSet(_securitySource);
+    }
+
+    function createVault() public returns (uint256){
+        uint256 vaultId = vaultCounter;
+
+        vaultCounter++;
+
+        _mint(address(this), vaultId);
+
+        address vaultAccount = registry.createHubChainAccount(_msgSender(), lzEndpoint);
+
+        VaultHubChainAccount(payable(vaultAccount)).initializeAccount(address(this), address(currencyToken), address(securitySource));
+
+        _transfer(address(this), msg.sender, vaultId);
+
+        vaultHubChainAccounts[vaultId] = vaultAccount;
+
+        emit VaultCreated(msg.sender, vaultId, vaultAccount);
+
+        return vaultId;
+
+    }
+
+
+    function setSpokeChainConfig(uint256 chainId, address spokeChainRegistry, uint32 spokeChainEid) public onlyOwner {
+        spokeChainsRegistries[chainId] = spokeChainRegistry;
+        spokeChainsIds[chainId] = spokeChainEid;
+
+        setPeer(spokeChainEid, bytes32(uint256(uint160(spokeChainRegistry))));
+    }
+
+    function _payNative(uint256 _nativeFee) internal override virtual returns (uint256 nativeFee) {
+        if (msg.value < _nativeFee) revert NotEnoughNative(msg.value);
+        return _nativeFee;
+    }
+
+
+    function createSpokeChainAccount(
+            uint256 vaultId, 
+            uint256 chainId, 
+            bytes calldata _extraSendOptions, // gas settings for A -> B
+            bytes calldata _extraReturnOptions
+        ) public payable {
+        
+        //require(_msgSender() == vaultHubChainAccounts[vaultId], "Only vault account can call this function");
+
+        uint128 GAS_LIMIT = 5000000; // Gas limit for the executor
+        uint128 MSG_VALUE = 10000000000000000; // msg.value for the lzReceive() function on destination in wei
+
+        
+
+        uint32 dstEid = spokeChainsIds[chainId];
+
+        bytes memory message = encodeMessage(_msgSender(),_extraReturnOptions);
+
+
+        //bytes memory options = OptionsBuilder.newOptions().addExecutorLzReceiveOption(GAS_LIMIT, MSG_VALUE);
+
+
+ 
+        bytes memory options = combineOptions(dstEid, SEND_ABA, _extraSendOptions);
+
+        //MessagingFee memory fee = _quote(dstEid, message, options, false);
+
+        MessagingReceipt memory receipt = _lzSend(
+            dstEid,
+            message,
+            options,
+            MessagingFee(msg.value, 0),
+            payable(ownerOf(vaultId))
+        );
+
+
+        emit SpokeChainRegistered(vaultId, chainId,receipt.nonce);
+    }
+
+    function quote(
+        uint32 _dstEid,
+        bytes memory _message,
+        bytes memory _options,
+        bool _payInLzToken
+    ) public view returns (MessagingFee memory fee) {
+        bytes memory payload = abi.encode(_message);
+        fee = _quote(_dstEid, payload, _options, _payInLzToken);
+    }
+
+
+    
+
+    function encodeMessage(
+        address hubChainAccount,
+        bytes memory _extraReturnOptions
+        ) public pure returns (bytes memory) {
+
+        uint256 extraOptionsLength = _extraReturnOptions.length;
+
+        return abi.encode(hubChainAccount, extraOptionsLength, _extraReturnOptions, extraOptionsLength);
+    }
+
+    function decodeMessage(bytes calldata encodedMessage) public pure returns (address hubChainAccount, address spokeChainAccount) {
+
+        (hubChainAccount, spokeChainAccount) = abi.decode(encodedMessage, (address, address));
+        
+        return (hubChainAccount, spokeChainAccount);
+    }
+
+
+    function _lzReceive(
+        Origin calldata _origin,
+        bytes32 /*_guid*/,
+        bytes calldata payload,
+        address /*_executor*/,
+        bytes calldata /*_extraData*/
+    ) internal override {
+
+        (address hubChainAccount, address newSpokeChainAccount) = decodeMessage(payload);
+
+        VaultHubChainAccount(payable(hubChainAccount)).registerNewSpokeChain(_origin.srcEid, newSpokeChainAccount);
+
+    }
+
+    
+     
+}
---
+++ quark-vaults/test/foundry/MultiVault.t.sol
@@ -0,0 +1,219 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import { VaultHubChainAccount } from "../../contracts/VaultHubChainAccount.sol";
+import { QuarkFactory } from "../../contracts/QuarkFactory.sol";
+import { SecuritySource } from "../../contracts/SecuritySource.sol";
+import { MockERC20 } from "../../contracts/mocks/MockERC20.sol";
+import { MockChainlinkDataFeed } from "../../contracts/mocks/MockChainlinkDataFeed.sol";
+
+import { TestHelperOz5 } from "@layerzerolabs/test-devtools-evm-foundry/contracts/TestHelperOz5.sol";
+import { RegistrySpokeChain } from "../../contracts/RegistrySpokeChain.sol";
+
+import { RegistryHubChain } from "../../contracts/RegistryHubChain.sol";
+
+import { OptionsBuilder } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
+
+contract VaultTest is TestHelperOz5 {
+
+    using OptionsBuilder for bytes;
+
+    QuarkFactory public factory;
+    MockERC20 public currency;
+    RegistryHubChain public registry;
+
+    SecuritySource public securitySource;
+
+    uint32 private aEid = 1;
+    uint32 private bEid = 2;
+
+
+    address private userA = address(0x1);
+    address private userB = address(0x2);
+
+    uint128 GAS_LIMIT_SEND = 5000000;  //Gas limit for the executor
+    uint128 MSG_VALUE_SEND = 10000000000000000; // msg.value for the lzReceive() function on destination in wei
+
+    uint128 GAS_LIMIT_RETURN = 3000000;
+    uint128 MSG_VALUE_RETURN = 0;
+    
+    function setUp() public override {
+
+        vm.deal(userA, 1000 ether);
+        vm.deal(userB, 1000 ether);
+
+        super.setUp();
+        setUpEndpoints(2, LibraryType.UltraLightNode);
+
+
+        currency = new MockERC20("USDC", "USDC");
+        securitySource = new SecuritySource(address(this));
+
+        registry = new RegistryHubChain(address(this));
+
+
+
+        console.log("Owner address: ", address(this));
+        factory = new QuarkFactory(address(this), address(registry), address(currency), endpoints[aEid]);
+        
+    }
+
+    // function test_createVault() public {
+    //     uint256 vaultId = factory.createVault();
+    //     //address account = factory.vaultHubChainAccounts(vaultId);
+
+    //     address owner = factory.ownerOf(vaultId);
+
+    //     assertEq(owner, address(this));
+    // }
+
+    // function test_sendEth() public {
+    //     uint256 vaultId = factory.createVault();
+    //     address payable account = payable(factory.vaultHubChainAccounts(vaultId));
+
+    //     uint256 balanceBefore = account.balance;
+    //     (bool success, ) = account.call{value: 0.0001 ether}("");
+
+    //     assertEq(success, true);
+    //     uint256 balanceAfter = account.balance;
+    //     assertEq(balanceAfter, balanceBefore + 0.0001 ether);
+
+    //     (success, ) = account.call{ value: 1 ether} (
+    //         abi.encodeWithSignature("execute(address,uint256,bytes,uint256)", vm.addr(1), 0.0000011 ether, "", 0)
+    //     );
+
+    //     assertEq(success, true);
+
+    // }
+
+    // function test_whitelistTokens() public {
+
+    //     MockERC20 token1 = new MockERC20("WBTC", "WBTC");
+    //     MockERC20 token2 = new MockERC20("USDC", "USDC");
+
+    //     //price feed mocked contracts
+    //     MockChainlinkDataFeed priceFeed1 = new MockChainlinkDataFeed(6306055000000); // BTC / USD prices 8 decimals
+    //     MockChainlinkDataFeed priceFeed2 = new MockChainlinkDataFeed(100000000); // USDC / USD prices 8 decimals
+
+    //     //volatility feed mocked contracts
+    //     MockChainlinkDataFeed volatilityFeed1 = new MockChainlinkDataFeed(67415); // BTC / USD  30 days volatitly 3 decimals on percentage -> 67415 = 67.415% vol
+    //     MockChainlinkDataFeed volatilityFeed2 = new MockChainlinkDataFeed(0); // USDC / USD  30 days volatitly 3 decimals on percentage
+
+    //     address[] memory tokens = new address[](2);
+    //     tokens[0] = address(token1);
+    //     tokens[1] = address(token2);
+
+    //     address[] memory priceFeeds = new address[](2);
+    //     priceFeeds[0] = address(priceFeed1);
+    //     priceFeeds[1] = address(priceFeed2);
+
+    //     address[] memory volatilityFeeds = new address[](2);
+    //     volatilityFeeds[0] = address(volatilityFeed1);
+    //     volatilityFeeds[1] = address(volatilityFeed2);
+
+
+    //     securitySource.setWhitelistedERC20Tokens(tokens, priceFeeds, volatilityFeeds);
+
+    //     assertEq(securitySource.numberWhitelistedERC20Tokens(), 2);
+
+    //     assertEq(securitySource.whitelistedERC20Tokens(0), address(token1));
+    //     assertEq(securitySource.whitelistedERC20Tokens(1), address(token2));
+
+    //     assertEq(securitySource.priceFeedsWhitelistedERC20Tokens(0), address(priceFeed1));
+    //     assertEq(securitySource.priceFeedsWhitelistedERC20Tokens(1), address(priceFeed2));
+
+    //     assertEq(securitySource.volatilityFeedsWhitelistedERC20Tokens(0), address(volatilityFeed1));
+    //     assertEq(securitySource.volatilityFeedsWhitelistedERC20Tokens(1), address(volatilityFeed2));
+    // }
+
+    // function test_initialDeposit() public {
+
+    //     uint256 vaultId = factory.createVault();
+    //     address account = factory.vaultHubChainAccounts(vaultId);
+
+    //     currency.mint(address(this), 1000 ether);
+    //     currency.approve(payable(account), 1000 ether);
+
+    //     VaultHubChainAccount(payable(account)).deposit(1000 ether);
+
+    //     uint256 price = 1;
+
+    //     assertEq(currency.balanceOf(account), 1000 ether);
+    //     assertEq(VaultHubChainAccount(payable(account)).balanceOf(address(this)), 1000 ether / price);
+    // }
+
+
+    // function test_depositAfterWBTCDeposit() public {
+
+    //     MockERC20 token1 = new MockERC20("WBTC", "WBTC");
+
+    //     //price feed mocked contracts
+    //     MockChainlinkDataFeed priceFeed1 = new MockChainlinkDataFeed(6306055000000); // BTC / USD prices 8 decimals
+    //     MockChainlinkDataFeed priceFeed2 = new MockChainlinkDataFeed(100000000); // USDC / USD prices 8 decimals
+
+    //     //volatility feed mocked contracts
+    //     MockChainlinkDataFeed volatilityFeed1 = new MockChainlinkDataFeed(67415); // BTC / USD  30 days volatitly 3 decimals on percentage -> 67415 = 67.415% vol
+    //     MockChainlinkDataFeed volatilityFeed2 = new MockChainlinkDataFeed(0); // USDC / USD  30 days volatitly 3 decimals on percentage
+
+    //     address[] memory tokens = new address[](2);
+    //     tokens[0] = address(token1);
+    //     tokens[1] = address(currency);
+
+    //     address[] memory priceFeeds = new address[](2);
+    //     priceFeeds[0] = address(priceFeed1);
+    //     priceFeeds[1] = address(priceFeed2);
+
+    //     address[] memory volatilityFeeds = new address[](2);
+    //     volatilityFeeds[0] = address(volatilityFeed1);
+    //     volatilityFeeds[1] = address(volatilityFeed2);
+
+    //     securitySource.setWhitelistedERC20Tokens(tokens, priceFeeds, volatilityFeeds);
+
+    //     factory.setSecuritySourceHubchain(address(securitySource));
+
+    //     uint256 vaultId = factory.createVault();
+    //     address account = factory.vaultHubChainAccounts(vaultId);
+
+    //     currency.mint(address(this), 1000 ether);
+    //     currency.approve(payable(account), 1000 ether);
+
+    //     VaultHubChainAccount(payable(account)).deposit(1000 ether);
+
+    //     uint256 price = 1;
+
+    //     assertEq(currency.balanceOf(account), 1000  ether);
+    //     assertEq(VaultHubChainAccount(payable(account)).balanceOf(address(this)), 1000 ether / price);
+
+    //     //Send WBTC to the account
+
+    //     token1.mint(account, 1 ether);
+
+    //     (uint256 amount, ) = VaultHubChainAccount(payable(account)).evaluateTotalValue();
+
+    //     uint256 amountExpected;
+
+    //     for(uint256 i = 0; i < tokens.length; i++) {
+    //         uint256 balance = MockERC20(tokens[i]).balanceOf(account);
+    //         (, int256 priceFromFeed, , , ) = MockChainlinkDataFeed(priceFeeds[i]).latestRoundData();
+    //         amountExpected += balance * uint256(priceFromFeed) / 10 ** MockERC20(tokens[i]).decimals();
+    //     }
+
+    //     assertEq(amount, amountExpected);
+    // }
+
+    function test_createSpokeChainAccount() public{
+
+        uint256 vaultId = factory.createVault();
+        address account = factory.vaultHubChainAccounts(vaultId);
+
+        bytes memory _optionsSend = OptionsBuilder.newOptions().addExecutorLzReceiveOption(GAS_LIMIT_SEND, MSG_VALUE_SEND);
+        bytes memory _optionsReturn = OptionsBuilder.newOptions().addExecutorLzReceiveOption(GAS_LIMIT_RETURN, MSG_VALUE_RETURN);
+        
+
+        factory.createSpokeChainAccount{value: 13000000005010644}(vaultId, 1, _optionsSend, _optionsReturn);
+
+
+        
+    }
+}
